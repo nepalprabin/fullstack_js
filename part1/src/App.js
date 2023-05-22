c09caba11e3d35425b627167c6e166786ec865af
Prabin Nepal <prabinnepal1996@gmail.com>
2023-05-21 22:05:53 -0500

    Component helper functions

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 
 function Profile(props) {
+  const workExperience = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.gradYear
+  }
   return (
     <div>
-    <p>{props.name} is the scientist from Britian</p>
-    <p>{props.name} is the scientist from US</p>
+    <p>{props.name} is the scientist from Britian. He has been working for {workExperience()} years</p>
     </div>
   )
 }
@@ -13,8 +16,7 @@ export default function App() {
   console.log('Get me some amazing scientists')
   return (
     <section>
-    <Profile name="Prabin" />
-    <Profile name="Nepal" />
+    <Profile name="Prabin" gradYear="2020" />
     </section>
   )
 }
