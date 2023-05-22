4cb726cec752b1a84db9241dace26d50625b2377
Prabin Nepal <prabinnepal1996@gmail.com>
2023-05-21 21:57:03 -0500

    Props

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 
-export default function Profile() {
+function Profile(props) {
   return (
-    <img
-    src="https://i.imgur.com/MK3eW3Am.jpg"
-    alt="Katherine Johnson"
-    />
+    <div>
+    <p>{props.name} is the scientist from Britian</p>
+    <p>{props.name} is the scientist from US</p>
+    </div>
+  )
+}
+
+export default function App() {
+  console.log('Get me some amazing scientists')
+  return (
+    <section>
+    <Profile name="Prabin" />
+    <Profile name="Nepal" />
+    </section>
   )
 }
